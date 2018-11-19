e9c46102a4aada564c3c7cf8548c0793b898f187
Nils Blach <nils.blach@gmx.de>
2018-11-19 17:55:43 +0100

    issues

--- code/AirplaneBoarding/actor.py
+++ code/AirplaneBoarding/actor.py
@@ -20,8 +20,10 @@ class Actor:
         self.passenger_type = passenger_type
         self.seat = seat
         self.action = starting_action
+        self.action_1 = 0
         self.position = -1
         self.luggage = passenger_type.number_of_bags
+        self.plane = plane
 
         # fields needed for switching
         self.switching = False
@@ -34,10 +36,14 @@ class Actor:
         on luggage
     seat := an object containing the row number and the column number of the assigned seat.
     action := an integer, which indicates the current action this actor is undertaking (possibilities can be found above
+    action_1 := indicating, which substate of action 1 is being done (0 = a, 1 = b, 2 = c)
     position := index of position in the aisle (-1 if not yet in plane/aisle)
+    luggage := current number of luggage pieces
     plane := plane object that is boarded. Containing aisle, etc.
+    
     """
 
+    # returns 1 if the actor is not in the plane yet
     def act(self):
 
             # treat switch separately
@@ -45,36 +51,38 @@ class Actor:
                 if(self.switch_timer > 0):
                     self.switch_timer -= 1
                 else:
-                    self.switch_move()
+                    self.set_position(self.position_after_switch)
                     self.switching = False
                 return 0
 
             else:
 
-                # if because python does not have proper switch statementsf
-                if(self.action = 0):
+                # if because python does not have proper switch statements
+                if self.action == 0:
 
                     # for action = 0, act is only called when self (this actor) is next in order to board plane
                     for i in range(0, self.size):
-                        if(self.plane.aisle.occupance[i] != 0):
-                            return 0 #cannot enter plane yet
+                        if self.plane.aisle.occupance[i] != 0:
+                            return 1 #cannot enter plane yet
 
                     # can enter plane
-                    self.position = 0;
-                    # TODO move in
+                    self.set_position(0)
+                    if self.luggage > 0:
+                        self.action = 1
+                    else:
+                        self.action = 3
+                    return 0
+
+                if self.action == 1:
+                    if self.action_1 == 0:
 
 
 
 
 
-
-
-        return
-
-
-    def switch_move(self):
-        self.position = self.position_after_switch
-
+    def set_position(self, position):
+        self.position = position
         # mark occupied space in aisle
-        for i in range(self.position, self.position+self.size):
-           self.plane.aisle.occupance[i] = self.id
+        for i in range(self.position, self.position + self.size):
+            self.plane.aisle.occupance[i] = self.id
+
