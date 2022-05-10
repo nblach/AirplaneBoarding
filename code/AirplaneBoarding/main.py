73114beeef7a3f202105c93ff8e17fdda47601c3
Nils Blach <nils.blach@gmx.de>
2022-05-11 00:04:41 +0200

    Version remaining changes

--- code/AirplaneBoarding/main.py
+++ code/AirplaneBoarding/main.py
@@ -2,11 +2,11 @@ from simulation import Simulation
 from plane import Plane
 from graphics import Animation
 
-plane = Plane(1, 0, 0, 0, 0, 0, 0, 0)
-sim = Simulation(125, plane, 80, 7, 1, 0, 0, 1)
+plane = Plane(2, 0, 0, 0, 0, 0, 0, 0)
+sim = Simulation(160, plane, 100, 7, 1, 0, 0, 1)
 sim.simulate()
 a = Animation(sim)
-a.animate(10)
+a.animate(20)
 
 
 
